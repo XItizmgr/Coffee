165a25410006286cc077de774df17f8915d02bf1
XItizmgr <xitizmgr1234567890@gmail.com>
2026-02-10 21:39:04 +0545

    login prb solved

--- script.js
+++ script.js
@@ -1,26 +1,7 @@
-const openBtn = document.querySelector(".book-now");
-const container = document.querySelector(".container");
 const ham = document.querySelector(".ham");
 const closeBtn = document.querySelector(".close");
 const navItems = document.querySelector(".nav-items");
-const login = document.querySelector(".login");
-const container2 = document.querySelector(".container2");
-openBtn.addEventListener("click", () => {
-  container.classList.add("show");
-});
-login.addEventListener("click", (event) => {
-  container2.classList.add("show");
-});
-window.addEventListener("keydown", (event) => {
-  if (event.key === "Escape") {
-    container.classList.remove("show");
-  }
-});
-container.addEventListener("click", (event) => {
-  if (event.target === container) {
-    container.classList.remove("show");
-  }
-});
+
 ham.addEventListener("click", () => {
   navItems.classList.add("active");
 });
@@ -31,4 +12,30 @@ window.addEventListener("keydown", (event) => {
   if (event.key === "Escape") {
     navItems.classList.remove("active");
   }
+});
+
+const openBtn = document.querySelector(".book-now");
+const container = document.querySelector(".container");
+const card1 = document.querySelector(".card");
+const login = document.querySelector(".login");
+const card2 = document.querySelector(".second-card");
+openBtn.addEventListener("click", () => {
+  container.classList.add("hidden");
+  card1.classList.add("show");
+});
+window.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    container.classList.remove("hidden");
+    card2.classList.remove("show");
+  }
+});
+container.addEventListener("click", (event) => {
+  if (event.target === container) {
+    container.classList.remove("hidden");
+    card2.classList.remove("show");
+  }
+});
+login.addEventListener("click", () => {
+  card1.classList.remove("show");
+  card2.classList.add("show");
 });
